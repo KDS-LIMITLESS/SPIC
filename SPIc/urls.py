85e232be8b891019f75112a7e9aa6269103cc12e
KDS-Limitless <s.kamahjnr@gmail.com>
2020-03-21 01:32:47 +0100

    SPIC ROUTES

--- SPIc/urls.py
+++ SPIc/urls.py
@@ -21,13 +21,22 @@ from django.conf.urls.static import static
 from user import views as userV
 from django.contrib.auth import views as authV
 
-
+                                                    # ROUTES
+                                                    
 urlpatterns = [
     path('register/', userV.register, name='register'),
+    path('profile/', userV.profile, name='profile'),
+
     path('login/', authV.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', authV.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
-    path('', blogV.index, name='index'),
-    path('profile', userV.profile, name='profile'),
+    
+    path('', blogV.PostListView.as_view(template_name='blog/index.html'), name='index'),
+
+    path('post/<int:pk>/', blogV.Post_Detail.as_view(template_name='blog/postDetail.html'), name='detail'),
+    path('post/new/', blogV.CreatePost.as_view(template_name='blog/newpost.html'), name='newpost'),
+    path('post/<int:pk>/update/', blogV.PostUpdateView.as_view(template_name='blog/newpost.html'), name='update'),
+    path('post/<int:pk>/delete/', blogV.DeletePost.as_view(template_name='blog/delete.html'), name='delete'),
+   
     path('admin/', admin.site.urls),    
 ]
 
