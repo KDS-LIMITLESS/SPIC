f8382dc952290a7a80eb10795a56e106173ff63b
KDS-Limitless <s.kamahjnr@gmail.com>
2020-03-21 01:28:32 +0100

    Updated post detail, post create, post edit and post delete function views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import PostItems
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -18,3 +19,37 @@ class PostListView(ListView):
     
 class Post_Detail(DetailView):
     model = PostItems
+
+class CreatePost(LoginRequiredMixin, CreateView):
+    model = PostItems
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = PostItems
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+ 
+class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = PostItems
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
